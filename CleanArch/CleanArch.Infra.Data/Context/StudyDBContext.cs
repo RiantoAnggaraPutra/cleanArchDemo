15f1f50f220e955a18d8a85bc3b38ea8a02b1fd2
rianto_anggara <riantoanggara08@gmail.com>
2022-01-03 21:50:25 +0700

    initial commit of infrastucture .Data.Proect

---
+++ CleanArch/CleanArch.Infra.Data/Context/StudyDBContext.cs
@@ -0,0 +1,25 @@
+﻿using CleanArch.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infra.Data.Context
+{
+    public class StudyDBContext : DbContext
+    {
+        public StudyDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
+
+namespace CleanArch.Infra.Data
+{
+    public class StudyDbContext
+    {
+    }
+}
