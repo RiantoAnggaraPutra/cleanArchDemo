1311ca1e6f2d943592ec12d4b7963108e817d82c
rianto_anggara <riantoanggara08@gmail.com>
2022-01-05 22:47:41 +0700

    Configure Presentation Layer  for Dependensy COntainer

---
+++ CleanArch/CleanArch.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,23 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.Services;
+using CleanArch.Domain.Interface;
+using CleanArch.Infra.Data.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServise (IServiceCollection services)
+        {
+            //Apllication Layer
+            services.AddScoped<ICourseServise, CourseServise>();
+
+            //Infra.Data Layer
+            services.AddScoped<ICourseRepository, CourseRepository>();  
+        }
+    }
+}
