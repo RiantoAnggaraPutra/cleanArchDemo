8a371f3f267672d0d0b997c050e99dfb972b4798
rianto_anggara <riantoanggara08@gmail.com>
2022-01-11 22:49:50 +0700

    commit of Authorize, views

---
+++ CleanArch/CleanArch.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.Mvc.Controllers
+{
+    [Authorize]
+    public class CourseController : Controller
+    {
+
+        private ICourseServise _courseServise;
+
+        public CourseController(ICourseServise courseService)
+        {
+            _courseServise = courseService;
+        }
+
+        public IActionResult Index()
+        {
+            CourseViewModel model = _courseServise.GetCourses();
+
+            return View(model);
+        }
+    }
+}
